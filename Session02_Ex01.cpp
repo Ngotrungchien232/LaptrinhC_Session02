627d2b963553830af15daf1e261cf8ae889385ae
Ngô Trung Chiến <ngotrungchien232@gmail.com>
2024-11-19 17:05:45 +0700

    C_Session02_Ex01

---
+++ Session02_Ex01.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+// thu vien chuan cho nhap xuat
+
+int main() {
+	// ham chinh diem bat dau cua chuong trinh
+	int a = 5;
+	// khai bao kieu so nguyen
+	char 'v';
+	// khai báo kieu ki tu
+	float pi = 3,14;
+	// khai bao kieu so thap phan
+	double pi = 3.141592653589793;
+	// gia tri gan dung cua so pi
+	printf();
+	// xuat ra ket qua ra man hinh
+	return 0;
+	// tra ve gia tri 0, bao chuong trinh ket thuc va thanh cong 
+}
+
+	
