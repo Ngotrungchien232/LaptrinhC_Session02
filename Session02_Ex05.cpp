22e3198bf1c8a73ed061dfe4ea54cc50b5a2c1db
Ngô Trung Chiến <ngotrungchien232@gmail.com>
2024-11-20 19:23:48 +0700

    C_Sesison02_Ex05

---
+++ Session02_Ex05.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+// day la khai bao thu vien chuan cua c
+
+int main() {
+	//tien hanh khai bao bien 
+	int length = 6, width = 4, chu_vi, dien_tich;
+	chu_vi = (length + width) * 2;
+	dien_tich = length * width;
+	printf("chu vi hinh chu nhat la: %d\n", chu_vi);
+	printf("dien tich hinh chu nhat la: %d", dien_tich);
+	return 0;
+}
