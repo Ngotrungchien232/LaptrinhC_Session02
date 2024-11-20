68612209b1bed3983059fead8b4df1b175c72525
Ngô Trung Chiến <ngotrungchien232@gmail.com>
2024-11-20 19:16:15 +0700

    C_Session02_Ex04

---
+++ Session02_Ex04.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+// khai bao thu vien
+
+int main() {
+	int edge = 3, chu_vi, dien_tich;
+	chu_vi = edge * 4;
+	dien_tich = edge * edge;
+	printf("chu vi hinh vuong la: %d\n", chu_vi);
+	printf("dien tich hinh vuong la: %d\n", dien_tich);
+	return 0;
+}
